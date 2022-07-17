6dc278c7690f420d4a75edebb58c8b729c189a76
Daniel Castro <danicc097@gmail.com>
2022-07-17 15:29:11 +0000

    dict type for json and jsonb

--- internal/codegen/python/postgresql_type.go
+++ internal/codegen/python/postgresql_type.go
@@ -20,7 +20,7 @@ func postgresType(req *plugin.CodeGenRequest, col *plugin.Column) string {
 	case "boolean", "bool", "pg_catalog.bool":
 		return "bool"
 	case "json", "jsonb":
-		return "Any"
+		return "dict"
 	case "bytea", "blob", "pg_catalog.bytea":
 		return "memoryview"
 	case "date":
